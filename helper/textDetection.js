3d8382e1be04052102e890042e60dfa70649c3ca
Daniel <tchatchouang.ouokam@gmail.com>
2021-12-27 10:21:30 +0100

    add View of the text extracted from an image

--- helper/textDetection.js
+++ helper/textDetection.js
@@ -1,9 +1,8 @@
 const fetch = require('cross-fetch');
 const config = {
     api : "https://vision.googleapis.com/v1/images:annotate?key=" ,
-    apiKey : "AIzaSyCxICIvCCPQ2lxGvDFfvldgUj3VCV7c9ag" ,
+    apiKey :  "AIzaSyA1uIaO7XMHMyYCiuEaLwEIuIQWnOb21nw",
 }
-
 
 const callGoogleVIsionApi = async (imageUrl) => {
 // Convert the image data to a Buffer and base64 encode it.
@@ -14,13 +13,16 @@ const callGoogleVIsionApi = async (imageUrl) => {
         method: 'POST',
         body: JSON.stringify({
             "requests": [
-                {
+                 {
                     "image":{
-                        "source":{
-                          "imageUri":
-                            imageUrl
-                        },
-                    },
+                        "content": base64
+                      },
+                    // "image":{
+                    //     "source":{
+                    //       "imageUri":
+                    //         imageUrl
+                    //     },
+                    // },
                     "features": [
                         { type: "TEXT_DETECTION", maxResults: 5 },
                         { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
@@ -31,13 +33,11 @@ const callGoogleVIsionApi = async (imageUrl) => {
     });
     const result = await googleVisionRes.json()
     const [text] = result.responses
-    //console.log(text);
     const fullText = text.fullTextAnnotation.text
-    console.log(fullText)
     return fullText;
 }
 
-//callGoogleVIsionApi("gs://image_assets_dis/img_test/img_test_1.png")
+callGoogleVIsionApi("gs://image_assets_dis/img_test/img_test_1.png")
 module.exports = {
     callGoogleVIsionApi
 }
